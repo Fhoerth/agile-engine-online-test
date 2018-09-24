41558ffe28455247282612aed518784111ca4674
Federico Hoerth <federicohoerth@gmail.com>
2018-09-24 08:05:02 -0300

    Render formats

--- src/app/modules/words-interaction/components/control-panel/index.ts
+++ src/app/modules/words-interaction/components/control-panel/index.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Format } from '../../interfaces/format';
+import { FormatService } from '../../services/format';
+
 @Component({
   selector: 'app-control-panel',
   templateUrl: './control-panel.component.html',
   styleUrls: ['./control-panel.component.css']
 })
 export class ControlPanelComponent implements OnInit {
-  constructor() {
-  }
+  formats: Format[];
+
+  constructor(private formatService: FormatService) {}
 
   ngOnInit() {
+    this.formats = this.formatService.formats;
   }
 }
