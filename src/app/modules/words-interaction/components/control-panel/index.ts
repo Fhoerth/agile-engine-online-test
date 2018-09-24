d12071e892d4f93226b4927d25ded735070ad639
Federico Hoerth <federicohoerth@gmail.com>
2018-09-24 08:42:03 -0300

    Emit Format

--- src/app/modules/words-interaction/components/control-panel/index.ts
+++ src/app/modules/words-interaction/components/control-panel/index.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 import { Format } from '../../interfaces/format';
 import { FormatService } from '../../services/format';
+
+import { Word } from '../../word';
 
 @Component({
   selector: 'app-control-panel',
@@ -9,11 +17,17 @@ import { FormatService } from '../../services/format';
   styleUrls: ['./control-panel.component.css']
 })
 export class ControlPanelComponent implements OnInit {
+  @Input() word: Word;
+  @Output() format = new EventEmitter<Format>();
   formats: Format[];
 
   constructor(private formatService: FormatService) {}
 
   ngOnInit() {
     this.formats = this.formatService.formats;
+  }
+
+  applyFormat(format: Format) {
+    this.format.emit(format);
   }
 }
--- src/app/modules/words-interaction/components/words-interaction/index.ts
+++ src/app/modules/words-interaction/components/words-interaction/index.ts
@@ -5,6 +5,8 @@ import {
   ViewChild,
 } from '@angular/core';
 
+import { Word } from '../../word';
+import { Format } from '../../interfaces/format';
 import { SelectionService } from '../../services/selection';
 
 @Component({
@@ -13,13 +15,21 @@ import { SelectionService } from '../../services/selection';
   styleUrls: ['./words-interaction.component.css'],
 })
 export class WordsInteractionComponent implements OnInit {
+  selectedWord: Word;
   @ViewChild('content') content: ElementRef;
 
   constructor(private selectionService: SelectionService) {
   }
 
   handleDblClick() {
-    console.log(this.selectionService.getSelectedWord(this.content));
+    const selectedWord = this.selectionService.getSelectedWord(this.content);
+
+    if (selectedWord) {
+      this.selectedWord = selectedWord;
+    }
+  }
+
+  onFormat(format: Format) {
   }
 
   ngOnInit() {
