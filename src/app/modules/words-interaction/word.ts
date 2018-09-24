45b8827310632a2e01cdb280ad755b9fdef2bd6f
Federico Hoerth <federicohoerth@gmail.com>
2018-09-24 08:59:45 -0300

    Add replaceNode method

--- src/app/modules/words-interaction/word.ts
+++ src/app/modules/words-interaction/word.ts
@@ -1,17 +1,30 @@
+import { FormatService } from './services/format';
+
 interface IOptions {
   content: string;
   range: Range;
+  formatService: FormatService;
 }
 
 export class Word {
-  content: String;
+  content: string;
   range: Range;
+  formatService: FormatService;
 
   constructor(options: IOptions) {
-    const { content, range } = options;
+    const { content, range, formatService } = options;
 
-    this.content = content;
+    this.formatService = formatService;
+    this.content = formatService.clean(content);
     this.range = range;
+  }
+
+  replaceNode(): void {
+    const contentDiv = document.createElement('div');
+    contentDiv.innerHTML = this.content;
+
+    this.range.deleteContents();
+    this.range.insertNode(contentDiv.childNodes[0]);
   }
 
   fetchSynonysms() {}
