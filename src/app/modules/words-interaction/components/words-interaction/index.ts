4ca890ecd5697f48e482a17b2ba7a4b6f896e413
Federico Hoerth <federicohoerth@gmail.com>
2018-09-24 08:06:38 -0300

    Rename files

--- src/app/modules/words-interaction/components/words-interaction/index.ts
+++ src/app/modules/words-interaction/components/words-interaction/index.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-words-interaction',
-  templateUrl: './control-panel.component.html',
-  styleUrls: ['./control-panel.component.css'],
+  templateUrl: './words-interaction.component.html',
+  styleUrls: ['./words-interaction.component.css'],
 })
 export class WordsInteractionComponent implements OnInit {
   constructor() {
