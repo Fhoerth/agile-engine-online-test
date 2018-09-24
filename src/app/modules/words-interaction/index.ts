0de7bb85ad6353e7f17da86ad83d2862092c9147
Federico Hoerth <federicohoerth@gmail.com>
2018-09-24 08:25:31 -0300

    Provide SelectionService

--- src/app/modules/words-interaction/index.ts
+++ src/app/modules/words-interaction/index.ts
@@ -24,6 +24,7 @@ import { SelectionService } from './services/selection';
   providers: [
     DataMuseAPIService,
     FormatService,
+    SelectionService,
   ],
 })
 export class WordsinteractionModule { }
